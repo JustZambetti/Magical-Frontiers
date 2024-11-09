d8f32dd4771dd15c6f3409a2e0d070dc3d41ece8
Zambetti Giorgio <giorgio.zambo2@gmail.com>
2024-11-09 09:49:41 +0100

    Initial commit

---
+++ src/main/java/com/zambettigiorgio/magicalfrontiers/MagicalFrontiersClient.java
@@ -0,0 +1,10 @@
+package com.zambettigiorgio.magicalfrontiers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicalFrontiersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
