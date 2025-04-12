c0b095b0be621b12b103cafba2fdd19166a0fd1f
Zambetti Giorgio <giorgio.zambo2@gmail.com>
2025-04-12 15:49:03 +0200

    feat: add bouncy mushroom

--- src/main/java/com/zambettigiorgio/magicalfrontiers/block/custom/BouncyMushroom.java
+++ src/main/java/com/zambettigiorgio/magicalfrontiers/block/custom/BouncyMushroom.java
@@ -4,8 +4,11 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
@@ -37,5 +40,40 @@ public class BouncyMushroom extends Block {
         return SHAPE;
     }
 
+    @Override
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
+        super.onLandedUpon(world, state, pos, entity, fallDistance * 0.5F);
+    }
 
+    @Override
+    public void onEntityLand(BlockView world, Entity entity) {
+        if (entity.bypassesLandingEffects()) {
+            super.onEntityLand(world, entity);
+        } else {
+            this.bounceEntity(entity);
+        }
+    }
+
+    private void bounceEntity(Entity entity) {
+        Vec3d vec3d = entity.getVelocity();
+        if (vec3d.y < 0.0) {
+            double d = entity instanceof LivingEntity ? 1.0 : 0.8;
+            entity.setVelocity(vec3d.x, -vec3d.y * 1F * d, vec3d.z);
+        }
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockPos belowPos = pos.down();
+        BlockState belowState = world.getBlockState(belowPos);
+        return Block.isFaceFullSquare(belowState.getCollisionShape(world, belowPos), Direction.UP);
+    }
+
+    @Override
+    protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, @Nullable WireOrientation wireOrientation, boolean notify) {
+        if (!canPlaceAt(state, world, pos)) {
+            world.breakBlock(pos, true);
+        }
+        super.neighborUpdate(state, world, pos, sourceBlock, wireOrientation, notify);
+    }
 }
